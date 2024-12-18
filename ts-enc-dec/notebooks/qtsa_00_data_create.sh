de3359515308944a835f2beb4dc82a2c580add6a
szajac <sebkaz@users.noreply.github.com>
2024-12-18 21:48:40 +0100

    fix branch

--- ts-enc-dec/notebooks/qtsa_00_data_create.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# A sequence of steps
-python qtsa_00_data_2sin_create_v1.2.py    log_3 -d 2_sins    -f Target_2_sins -st 70 -sv 30       -rs 1410
-python qtsa_00_data_2sin_sw_create_v1.2.py log_3 -d 2_sins_sw -f Target_2_sins -st 70 -sv 30 -wn 5 -rs 2024
